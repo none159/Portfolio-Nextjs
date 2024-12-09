6681e08a0d801cdd7f7d5f70a76ead8a396a19be
none159 <noneenone019@gmail.com>
2024-12-09 22:21:07 +0100

    update

--- src/app/components/aboutme.tsx
+++ src/app/components/aboutme.tsx
@@ -9,8 +9,11 @@ function Aboutme() {
             </div>
             <div className='grid gap-10  w-[500px] '>
                 <h2 className='text-5xl'>About Me</h2>
-                <h3 className='text-xl leading-2'>Lorem ipsum, dolor sit amet consectetur
-                     adipisicing elit. Sapiente esse quia iure numquam assumenda possimus unde facilis pariatur optio deserunt. Rem cupiditate maiores nemo enim distinctio vitae ullam ut eum.</h3>
+                <h3 className='text-xl leading-2'>Hi, I&apos;m Yassine Mouhib, a passionate and dedicated web developer with a strong foundation in modern technologies and a continuous drive to learn and grow. Over the years, I've honed my skills by working on diverse projects, from crafting responsive websites and interactive applications to integrating seamless user experiences. My expertise lies in leveraging tools like React, Next.js, and various backend solutions to bring ideas to life.
+
+Beyond coding, I thrive on solving problems and collaborating with others to create meaningful digital solutions. I&apos;m always open to new challenges and opportunities to make a difference through innovation. Whether it's a complex technical hurdle or a creative project idea, I approach every task with enthusiasm and commitment.
+
+When I&apos;m not coding, you might find me exploring new technologies, sharing knowledge with others, or diving into my next project with full energy and passion.</h3>
              <div className='grid gap-5'>
                 <h4 className='border-b-2 border-blue-600  w-fit '>Skills</h4>
                 <ul className='grid gap-1 '>
